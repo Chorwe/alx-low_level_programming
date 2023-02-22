947fab90224720ab767f11fb339aae9f48cf7bef
chorwe <erickkirui007@gmail.com>
2023-02-22 10:13:15 -0800

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,22 @@
 #include "main.h"
 
 int main() {
-    _putchar '_';
-    _putchar 'p';
-    _putchar 'u';
-    _putchar 't';
-    _putchar 'c';
-    _putchar 'h';
-    _putchar 'a';
-    _putchar 'r';
+
+    _putchar('_');
+
+    _putchar('p');
+
+    _putchar('u');
+
+    _putchar('t');
+
+    _putchar('c');
+
+    _putchar('h');
+
+    _putchar('a');
+
+    _putchar('r');
 
     return 0;
 }
