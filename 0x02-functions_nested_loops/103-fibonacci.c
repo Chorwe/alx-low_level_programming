9edcd7ee41dd2bc8fe2710cbc29d0eb2b3318f50
chorwe <erickkirui007@gmail.com>
2023-02-22 18:40:25 -0800

    .

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,13 +2,13 @@
 
 int main() {
     int prev = 1, curr = 2, sum = 0;
-    while (curr <= 4000000) {
-        if (curr % 2 == 0) {
-            sum += curr;
-        }
+    while (curr < 4000000) {
         int temp = curr;
         curr += prev;
         prev = temp;
+        if (prev % 2 == 0) {
+            sum += prev;
+        }
     }
     printf("%d\n", sum);
     return 0;
