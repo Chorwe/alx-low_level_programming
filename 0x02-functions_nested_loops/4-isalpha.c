d1d719e320a13daf8cd6ea64197f28eea5f1a1ee
chorwe <erickkirui007@gmail.com>
2023-02-22 17:40:14 -0800

    .

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,9 +1,11 @@
 #include "main.h"
 
-int _islower(int c)
+/**
+ * _isalpha - checks for alphabetic character
+ */
+int _isalpha(int c)
 {
-    if (c >= 'a' && c <= 'z')
-        return 1;
-    else
-        return 0;
+        if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+                return (1);
+        return (0);
 }
