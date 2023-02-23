d9882056f991def9303d0e1342c58f40232832f0
chorwe <erickkirui007@gmail.com>
2023-02-23 10:17:10 -0800

    .

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    char c;
+
+    printf("Enter a character: ");
+    scanf("%c", &c);
+
+    if (_isupper(c))
+        printf("The character %c is uppercase.\n", c);
+    else
+        printf("The character %c is not uppercase.\n", c);
+
+    return (0);
+}
