5ca88d0f3a74f477e1781ec50c835ca5b80d233b
chorwe <erickkirui007@gmail.com>
2023-02-22 18:13:29 -0800

    .

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * print_to_98
+ */
+void print_to_98(int n)
+{
+    int i;
+
+    if (n <= 98)
+    {
+        for (i = n; i < 98; i++)
+        {
+            printf("%d, ", i);
+        }
+    }
+    else
+    {
+        for (i = n; i > 98; i--)
+        {
+            printf("%d, ", i);
+        }
+    }
+
+    printf("98\n");
+}
