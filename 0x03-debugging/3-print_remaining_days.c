db6ab3524576441e33ccbdf6dc258f3f7347f2c6
Erick kirui <122808348+Chorwe@users.noreply.github.com>
2023-02-25 05:08:40 +0300

    Update 3-print_remaining_days.c
    u

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -1,26 +1,91 @@
-void print_remaining_days(int month, int day, int year)
-{
-    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+#include <stdio.h>
+#include "main.h"
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
+// Function to determine if a given year is a leap year
+int isLeapYear(int year) {
+    if (year % 400 == 0) {
+        return 1;
     }
-    else
-    {
-        if (month == 2 && day == 60)
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
+    if (year % 100 == 0) {
+        return 0;
     }
+    if (year % 4 == 0) {
+        return 1;
+    }
+    return 0;
+}
+
+// Function to determine the number of days in a given month
+int getDaysInMonth(int month, int year) {
+    int daysInMonth;
+    switch (month) {
+        case 2:
+            daysInMonth = isLeapYear(year) ? 29 : 28;
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            daysInMonth = 30;
+            break;
+        default:
+            daysInMonth = 31;
+    }
+    return daysInMonth;
+}
+
+// Function to validate the input date
+int isValidDate(int day, int month, int year) {
+    if (year < 1 || year > 9999) {
+        return 0;
+    }
+    if (month < 1 || month > 12) {
+        return 0;
+    }
+    int daysInMonth = getDaysInMonth(month, year);
+    if (day < 1 || day > daysInMonth) {
+        return 0;
+    }
+    return 1;
+}
+
+// Function to calculate the next date
+void getNextDate(int day, int month, int year, int* nextDay, int* nextMonth, int* nextYear) {
+    // If the input date is not valid, set the output date to 0/0/0
+    if (!isValidDate(day, month, year)) {
+        *nextDay = 0;
+        *nextMonth = 0;
+        *nextYear = 0;
+        return;
+    }
+    // If the input date is the last day of the year, set the output date to 1/1/next year
+    if (day == 31 && month == 12) {
+        *nextDay = 1;
+        *nextMonth = 1;
+        *nextYear = year + 1;
+        return;
+    }
+    int daysInMonth = getDaysInMonth(month, year);
+    // If the input date is the last day of the month, set the output date to 1/next month/this year
+    if (day == daysInMonth) {
+        *nextDay = 1;
+        *nextMonth = month + 1;
+        *nextYear = year;
+        return;
+    }
+    // Otherwise, set the output date to next day/this month/this year
+    *nextDay = day + 1;
+    *nextMonth = month;
+    *nextYear = year;
+}
+
+// Main function for testing
+int main() {
+    int day, month, year;
+    printf("Enter a date (in the format DD/MM/YYYY): ");
+    scanf("%d/%d/%d", &day, &month, &year);
+    int nextDay, nextMonth, nextYear;
+    getNextDate(day, month, year, &nextDay, &nextMonth, &nextYear);
+    printf("The next date is: %d/%d/%d\n", nextDay, nextMonth, nextYear);
+    return 0;
 }
