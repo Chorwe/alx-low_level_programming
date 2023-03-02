65169621d0fe69cbbcee727732337b0ad9241b14
chorwe <erickkirui007@gmail.com>
2023-03-02 14:09:38 -0800

    .

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - Capitalizes all words of a string
+ * @str: The input string
+ *
+ * Return: Pointer to the modified string
+ */
+char *cap_string(char *str)
+{
+    int i;
+
+    /* Capitalize first character */
+    if (str[0] >= 'a' && str[0] <= 'z')
+        str[0] -= 32;
+
+    for (i = 1; str[i] != '\0'; i++)
+    {
+        /* Capitalize first character of each word */
+        if ((str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+             str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+             str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+             str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+             str[i - 1] == '}') && (str[i] >= 'a' && str[i] <= 'z'))
+            str[i] -= 32;
+    }
+
+    return (str);
+}
