b0bebc046527e2ccee8dd7abb947559a14ab053e
chorwe <erickkirui007@gmail.com>
2023-03-21 14:05:49 -0700

    .

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,25 +1,56 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
 
+/**
+ * new_dog - creates a new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: pointer to the new dog, or NULL on failure
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *new_dog = malloc(sizeof(dog_t));
-    if (new_dog == NULL) {
-        return NULL; // Allocation failed
-    }
-    // Allocate memory for name and owner strings
-    new_dog->name = malloc(strlen(name) + 1);
-    new_dog->owner = malloc(strlen(owner) + 1);
-    if (new_dog->name == NULL || new_dog->owner == NULL) {
-        free(new_dog->name);
-        free(new_dog->owner);
+    dog_t *new_dog;
+    char *new_name, *new_owner;
+    int name_len, owner_len;
+
+    /* allocate memory for the new dog */
+    new_dog = malloc(sizeof(dog_t));
+    if (new_dog == NULL)
+        return (NULL);
+
+    /* allocate memory for the name */
+    for (name_len = 0; name[name_len] != '\0'; name_len++)
+        ;
+    new_name = malloc(name_len + 1);
+    if (new_name == NULL)
+    {
         free(new_dog);
-        return NULL; // Allocation failed
+        return (NULL);
     }
-    // Copy name and owner strings to allocated memory
-    strcpy(new_dog->name, name);
-    strcpy(new_dog->owner, owner);
+
+    /* allocate memory for the owner */
+    for (owner_len = 0; owner[owner_len] != '\0'; owner_len++)
+        ;
+    new_owner = malloc(owner_len + 1);
+    if (new_owner == NULL)
+    {
+        free(new_name);
+        free(new_dog);
+        return (NULL);
+    }
+
+    /* copy the name and owner */
+    for (int i = 0; i <= name_len; i++)
+        new_name[i] = name[i];
+    for (int i = 0; i <= owner_len; i++)
+        new_owner[i] = owner[i];
+
+    /* assign values to the new dog */
+    new_dog->name = new_name;
     new_dog->age = age;
-    return new_dog;
+    new_dog->owner = new_owner;
+
+    return (new_dog);
 }
