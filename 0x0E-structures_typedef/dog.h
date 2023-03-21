53de65f3150dc4319241e45034082324d446daef
chorwe <erickkirui007@gmail.com>
2023-03-21 13:54:08 -0700

    .

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,1 +1,25 @@
-typedef struct dog dog_t;
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure for dogs
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: structure called dog containing information about a dog
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+
+/* function prototypes */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+
+#endif /* DOG_H */
